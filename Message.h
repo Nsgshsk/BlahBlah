23900d21aec06ed7078949713503469722aaa1f5
Lubomir Topalski <lubomirtopalski@gmail.com>
2025-05-04 12:22:36 +0300

    Started work on Message data type

---
+++ Message.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+class Message
+{
+    char* sender_;
+    char dateTime_[32];
+    char* message_;
+
+    void copyFrom(const Message& message);
+    void moveFrom(Message&& message);
+    void free();
+
+public:
+    Message();
+    Message(const char* sender, const char* message);
+    Message(const Message& other);
+    Message& operator=(const Message& other);
+    ~Message();
+
+    Message(Message&& other) noexcept;
+    Message& operator=(Message&& other) noexcept;
+
+    const char* getSender() const;
+    const char* getDateTime() const;
+    const char* getMessage() const;
+};
