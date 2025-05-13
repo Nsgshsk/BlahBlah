6fac95c8ce7761454606f789731ea3b4f6a527b4
Lubomir Topalski <lubomirtopalski@gmail.com>
2025-05-13 18:51:34 +0300

    Created UserBase that will be helpful with User and Chat implementation

---
+++ UserBase.cpp
@@ -0,0 +1,74 @@
+﻿#include "UserBase.h"
+
+#include <algorithm>
+#include <cstring>
+
+void UserBase::copyFrom(const char* id, const char* name)
+{
+    strcpy_s(this->id_, id);
+    size_t temp = strlen(name);
+    this->name_ = new char[temp + 1];
+    strcpy_s(this->name_, temp + 1, name);
+}
+
+void UserBase::copyFrom(const UserBase& other)
+{
+    copyFrom(other.id_, other.name_);
+}
+
+void UserBase::moveFrom(UserBase&& other)
+{
+    strcpy_s(this->id_, other.id_);
+    this->name_ = other.name_;
+    other.name_ = nullptr;
+}
+
+void UserBase::free()
+{
+    id_[0] = '\0';
+    delete[] name_;
+}
+
+UserBase::UserBase() : id_{}, name_(nullptr) 
+{
+}
+
+UserBase::UserBase(const char* id, const char* name)
+{
+    copyFrom(id, name);
+}
+
+UserBase::UserBase(const UserBase& other)
+{
+    copyFrom(other);
+}
+
+UserBase& UserBase::operator=(const UserBase& other)
+{
+    if (this != &other)
+    {
+        free();
+        copyFrom(other);
+    }
+    return *this;
+}
+
+UserBase::~UserBase()
+{
+    free();
+}
+
+UserBase::UserBase(UserBase&& other)
+{
+    moveFrom(std::move(other));
+}
+
+UserBase& UserBase::operator=(UserBase&& other)
+{
+    if (this != &other)
+    {
+        free();
+        moveFrom(std::move(other));
+    }
+    return *this;
+}
---
+++ UserBase.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+class UserBase
+{
+    char id_[16];
+    char* name_;
+
+    void copyFrom(const char* id, const char* name);
+    void copyFrom(const UserBase& other);
+    void moveFrom(UserBase&& other);
+    void free();
+public:
+    UserBase();
+    UserBase(const char* id, const char* name);
+    UserBase(const UserBase& other);
+    UserBase& operator=(const UserBase& other);
+    ~UserBase();
+    
+    UserBase(UserBase&& other) noexcept;
+    UserBase& operator=(UserBase&& other) noexcept;
+};
