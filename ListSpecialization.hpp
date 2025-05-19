a678dd35ae9caaa3a1c6cbcb066b5c1efcd73fc8
Lubomir Topalski <lubomirtopalski@gmail.com>
2025-05-20 00:24:43 +0300

    Updated List.hpp and made a ListSpecialization.hpp for list pointer specialization to avoid using triple pointers

--- ListSpecialization.hpp
+++ ListSpecialization.hpp
@@ -1,10 +1,7 @@
 ﻿#pragma once
-#include <iostream>
-
-constexpr uint8_t DEFAULT_LIST_CAPACITY = 16;
 
 template<class T>
-class List
+class List<T*>
 {
     T** data_;
     size_t size_;
@@ -18,7 +15,7 @@ class List
     void free();
     void resize(size_t newSize);
     void resize();
-    
+
 public:
     List();
     List(const List& other);
@@ -28,24 +25,22 @@ public:
     List(List&& other) noexcept;
     List& operator=(List&& other) noexcept;
 
-    const T& operator[](size_t index) const;
-    T& operator[](size_t index);
+    const T* operator[](size_t index) const;
+    T*& operator[](size_t index);
 
     size_t getSize() const;
     size_t getCapacity() const;
 
-    void add(const T& item);
+    void add(T* item);
     //void remove(const T& item);
 
-    void insertAt(size_t index, const T& item);
+    void insertAt(size_t index, T* item);
     void removeAt(size_t index);
 
     void pop();
     void clear();
 
     void append(const List& list);
-    void append(List&& list) noexcept;
-    
     void reverse();
 
     void reserve(size_t newCapacity);
@@ -54,14 +49,14 @@ public:
     friend std::ostream& operator<<(std::ostream& os, const List& other)
     {
         for (size_t i = 0; i < other.size_; i++)
-            os << other[i] << '\n';
+            os << *other[i] << '\n';
 
         return os;
     }
 };
 
 template <class T>
-void List<T>::reorder_left(size_t pos)
+void List<T*>::reorder_left(size_t pos)
 {
     for (size_t i = pos; i < size_ - 1; i++)
         data_[i] = data_[i + 1];
@@ -70,24 +65,24 @@ void List<T>::reorder_left(size_t pos)
 }
 
 template <class T>
-void List<T>::reorder_right(size_t pos)
+void List<T*>::reorder_right(size_t pos)
 {
     for (size_t i = size_ - 1; i >= pos; i--)
         data_[i + 1] = data_[i];
 }
 
 template <class T>
-void List<T>::copyFrom(const List& other)
+void List<T*>::copyFrom(const List& other)
 {
     for (size_t i = 0; i < other.size_; i++)
-        data_[i] = new T(other[i]);
+        data_[i] = other.data_[i];
 
     size_ = other.size_;
     capacity_ = other.capacity_;
 }
 
 template <class T>
-void List<T>::moveFrom(List&& other)
+void List<T*>::moveFrom(List&& other)
 {
     data_ = other.data_;
     size_ = other.size_;
@@ -99,12 +94,8 @@ void List<T>::moveFrom(List&& other)
 }
 
 template <class T>
-void List<T>::free()
+void List<T*>::free()
 {
-    for (size_t i = 0; i < capacity_; i++)
-        if (data_[i] != nullptr)
-            delete data_[i];
-    
     delete[] data_;
     data_ = nullptr;
     size_ = 0;
@@ -112,39 +103,38 @@ void List<T>::free()
 }
 
 template <class T>
-void List<T>::resize(size_t newSize)
+void List<T*>::resize(size_t newSize)
 {
     T** newData = new T*[newSize]{nullptr};
     for (size_t i = 0; i < size_; i++)
         newData[i] = data_[i];
 
-    if (data_ != nullptr)
-        delete[] data_;
+    free();
     
     data_ = newData;
     capacity_ = newSize;
 }
 
 template <class T>
-void List<T>::resize()
+void List<T*>::resize()
 {
     resize(2 * capacity_);
 }
 
 template <class T>
-List<T>::List() : size_(0), capacity_(DEFAULT_LIST_CAPACITY)
+List<T*>::List(): size_(0)
 {
-    data_ = new T*[DEFAULT_LIST_CAPACITY]{nullptr};
+    resize(DEFAULT_LIST_CAPACITY);
 }
 
 template <class T>
-List<T>::List(const List& other)
+List<T*>::List(const List& other)
 {
     copyFrom(other);
 }
 
 template <class T>
-List<T>& List<T>::operator=(const List& other)
+List<T*>& List<T*>::operator=(const List& other)
 {
     if (this != &other)
     {
@@ -155,19 +145,19 @@ List<T>& List<T>::operator=(const List& other)
 }
 
 template <class T>
-List<T>::~List()
+List<T*>::~List()
 {
     free();
 }
 
 template <class T>
-List<T>::List(List&& other) noexcept
+List<T*>::List(List&& other) noexcept
 {
     moveFrom(std::move(other));
 }
 
 template <class T>
-List<T>& List<T>::operator=(List&& other) noexcept
+List<T*>& List<T*>::operator=(List&& other) noexcept
 {
     if (this != &other)
     {
@@ -178,41 +168,41 @@ List<T>& List<T>::operator=(List&& other) noexcept
 }
 
 template <class T>
-const T& List<T>::operator[](size_t index) const
+const T* List<T*>::operator[](size_t index) const
 {
-    return *data_[index];
+    return data_[index];
 }
 
 template <class T>
-T& List<T>::operator[](size_t index)
+T*& List<T*>::operator[](size_t index)
 {
-    return *data_[index];
+    return data_[index];
 }
 
 template <class T>
-size_t List<T>::getSize() const
+size_t List<T*>::getSize() const
 {
     return size_;
 }
 
 template <class T>
-size_t List<T>::getCapacity() const
+size_t List<T*>::getCapacity() const
 {
     return capacity_;
 }
 
 template <class T>
-void List<T>::add(const T& item)
+void List<T*>::add(T* item)
 {
     if (size_ + 1 > capacity_)
         resize();
 
-    data_[size_] = new T(item);
+    data_[size_] = item;
     size_++;
 }
 
 template <class T>
-void List<T>::insertAt(size_t index, const T& item)
+void List<T*>::insertAt(size_t index, T* item)
 {
     if (index >= size_)
         throw std::out_of_range("Index is out of range");
@@ -221,67 +211,46 @@ void List<T>::insertAt(size_t index, const T& item)
         resize();
 
     reorder_right(index);
-    data_[index] = new T(item);
+    data_[index] = item;
     size_++;
 }
 
 template <class T>
-void List<T>::removeAt(size_t index)
+void List<T*>::removeAt(size_t index)
 {
     if (index >= size_)
         throw std::out_of_range("Index is out of range");
-
-    delete data_[index];
+    
     reorder_left(index);
     size_--;
 }
 
 template <class T>
-void List<T>::pop()
+void List<T*>::pop()
 {
     if (size_ == 0)
         throw std::out_of_range("List is empty");
-
-    delete data_[0];
+    
     reorder_left(0);
     size_--;
 }
 
 template <class T>
-void List<T>::clear()
+void List<T*>::clear()
 {
     free();
     reserve(DEFAULT_LIST_CAPACITY);
 }
 
 template <class T>
-void List<T>::append(const List& list)
+void List<T*>::append(const List& list)
 {
     for (size_t i = 0; i < list.size_; i++)
         this->add(list[i]);
 }
 
 template <class T>
-void List<T>::append(List&& list) noexcept
-{
-    if (this == &list)
-    {
-        append(list);
-        return;
-    }
-    
-    while (size_ + list.size_ > capacity_)
-        resize();
-    
-    for (size_t i = 0; i < list.size_; i++)
-    {
-        data_[size_ + i] = list.data_[i];
-        list.data_[i] = nullptr;
-    }
-}
-
-template <class T>
-void List<T>::reverse()
+void List<T*>::reverse()
 {
     for (size_t i = 0; i < size_ / 2; i++)
     {
@@ -292,13 +261,13 @@ void List<T>::reverse()
 }
 
 template <class T>
-void List<T>::reserve(size_t newCapacity)
+void List<T*>::reserve(size_t newCapacity)
 {
     resize(newCapacity);
 }
 
 template <class T>
-void List<T>::fitToSize()
+void List<T*>::fitToSize()
 {
     resize(size_);
 }
